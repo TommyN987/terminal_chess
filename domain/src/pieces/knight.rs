1f5b2c04f36a772851c8e9b3ade3c03971c591ca
TommyN987 <tomas.nagy.tn@gmail.com>
2024-11-14 12:33:39 +0100

    test: unit tests for knight

--- domain/src/pieces/knight.rs
+++ domain/src/pieces/knight.rs
@@ -45,3 +45,97 @@ impl Knight {
             .collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::pieces::{Pawn, Piece, PieceType};
+
+    use super::*;
+
+    #[test]
+    fn test_knight_unblocked_moves() {
+        // Arrange
+
+        let mut board = Board::default();
+        let knight_position = Position::from((4, 4));
+        let knight = Knight;
+
+        board.set(
+            &knight_position,
+            Some(Piece::new(PieceType::Knight(Knight), Color::White)),
+        );
+
+        // Act
+        let moves = knight.get_moves(Color::White, true, knight_position, &board);
+
+        // Assert
+        let expected_position = vec![
+            Position::from((3, 2)),
+            Position::from((5, 2)),
+            Position::from((6, 3)),
+            Position::from((6, 5)),
+            Position::from((5, 6)),
+            Position::from((3, 6)),
+            Position::from((2, 5)),
+            Position::from((2, 3)),
+        ];
+
+        let result_positions: Vec<_> = moves.iter().map(|m| m.to).collect();
+
+        assert_eq!(result_positions.len(), expected_position.len());
+
+        for pos in expected_position {
+            assert!(
+                result_positions.contains(&pos),
+                "Expected position {:?} not found in moves",
+                pos
+            );
+        }
+    }
+
+    #[test]
+    fn test_knight_blocked_by_same_color() {
+        // Arrange
+        let board = Board::new();
+        let knight = Knight;
+
+        // Act
+        let result_positions: Vec<_> = knight
+            .get_moves(Color::White, false, Position::from((7, 1)), &board)
+            .into_iter()
+            .map(|m| m.to)
+            .collect();
+
+        // Assert
+        assert!(!result_positions.contains(&Position::from((6, 3))));
+    }
+
+    #[test]
+    fn test_knight_can_capture_opponent_piece() {
+        // Arrange
+        let mut board = Board::new();
+        let knight = Knight;
+        let knight_position = Position::from((7, 1));
+        let opponent_position = Position::from((6, 3));
+
+        board.set(
+            &opponent_position,
+            Some(Piece::new(
+                PieceType::Pawn(Pawn::new(Direction::South)),
+                Color::Black,
+            )),
+        );
+
+        // Act
+        let result_positions: Vec<_> = knight
+            .get_moves(Color::White, false, knight_position, &board)
+            .into_iter()
+            .map(|m| m.to)
+            .collect();
+
+        assert!(
+            result_positions.contains(&opponent_position),
+            "Knight should be able to capture at (6, 3)"
+        );
+    }
+}
