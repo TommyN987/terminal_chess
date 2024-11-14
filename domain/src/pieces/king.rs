94c13cefdfd6c4f57c72b4c71f566139637227dc
TommyN987 <tomas.nagy.tn@gmail.com>
2024-11-14 12:48:58 +0100

    test: unit tests for king

--- domain/src/pieces/king.rs
+++ domain/src/pieces/king.rs
@@ -21,7 +21,7 @@ impl King {
         Self {
             directions: [
                 Direction::North,
-                Direction::North,
+                Direction::South,
                 Direction::East,
                 Direction::West,
                 Direction::NorthEast,
@@ -82,5 +82,100 @@ impl King {
                 }
             })
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::pieces::{Knight, Piece};
+
+    use super::*;
+
+    #[test]
+    fn test_king_unblocked_moves() {
+        // Arrange
+        let mut board = Board::default();
+        let king = King::new();
+        let king_position = Position::from((4, 4));
+
+        board.set(
+            &king_position,
+            Some(Piece::new(PieceType::King(King::new()), Color::White)),
+        );
+
+        // Act
+        let moves = king.get_moves(Color::White, true, king_position, &board);
+
+        // Assert
+        let expected_positions = vec![
+            Position::from((4, 3)),
+            Position::from((4, 5)),
+            Position::from((3, 3)),
+            Position::from((3, 4)),
+            Position::from((3, 5)),
+            Position::from((5, 3)),
+            Position::from((5, 4)),
+            Position::from((5, 5)),
+        ];
+
+        let result_positions: Vec<_> = moves.iter().map(|m| m.to).collect();
+
+        assert_eq!(result_positions.len(), expected_positions.len());
+
+        for pos in expected_positions {
+            assert!(result_positions.contains(&pos));
+        }
+    }
+
+    #[test]
+    fn test_king_blocked_by_same_color() {
+        let board = Board::new();
+        let king = King::new();
+        let king_position = Position::from((7, 4));
+
+        let moves = king.get_moves(Color::White, false, king_position, &board);
+
+        assert!(moves.is_empty());
+    }
+
+    #[test]
+    fn test_king_can_capture_opponent_piece() {
+        let mut board = Board::default();
+        let king = King::new();
+        let king_position = Position::from((4, 4));
+        let opponent_position = Position::from((4, 3));
+
+        board.set(
+            &king_position,
+            Some(Piece::new(PieceType::King(King::new()), Color::White)),
+        );
+
+        board.set(
+            &opponent_position,
+            Some(Piece::new(PieceType::Knight(Knight), Color::Black)),
+        );
+
+        // Act
+        let moves = king.get_moves(Color::White, true, king_position, &board);
+
+        // Assert
+        let expected_positions = vec![
+            Position::from((4, 3)),
+            Position::from((4, 5)),
+            Position::from((3, 3)),
+            Position::from((3, 4)),
+            Position::from((3, 5)),
+            Position::from((5, 3)),
+            Position::from((5, 4)),
+            Position::from((5, 5)),
+        ];
+
+        let result_positions: Vec<_> = moves.iter().map(|m| m.to).collect();
+
+        assert_eq!(result_positions.len(), expected_positions.len());
+
+        for pos in expected_positions {
+            assert!(result_positions.contains(&pos));
+        }
     }
 }
