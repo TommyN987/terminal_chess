3833236c6fd85d0b7a253d73d33cb551a108f2fe
Tommy Nagy <96293671+TommyN987@users.noreply.github.com>
2024-12-05 22:28:06 +0100

    feat/protocol (#3)

---
+++ protocol/src/constants.rs
@@ -0,0 +1,5 @@
+pub const VERSION: u8 = 1;
+pub const HEADER_SIZE: usize = 4;
+pub const PACKET_MAX_SIZE: usize = 1024;
+pub const TYPE_ENC_INDEX: usize = 1;
+pub const HEADER_LENGTH_OFFSET: usize = 2;
